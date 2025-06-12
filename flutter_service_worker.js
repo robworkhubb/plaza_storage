f3f69d2537bcdcc5fd581285153ccd197994e8ed
Roberto <robworkhub@gmail.com>
2025-06-12 19:19:47 +0200

    Rebuild and fix manifest + bootstrap

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "7d0459dfb8c8ecff319b2d77f4a7b03c
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "c55ba6fef8bc677b9daf3d4038256244",
+"flutter_bootstrap.js": "19385a011ac86f7fab9082a9ca0fcd82",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
